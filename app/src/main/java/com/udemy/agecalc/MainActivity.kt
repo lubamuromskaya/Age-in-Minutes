e9330d954855891ed1d8cb597858a96de1379697
lubamur <lubamuromskaya@gmail.com>
2021-11-01 16:31:53 +0300

    forbidden future dates

--- app/src/main/java/com/udemy/agecalc/MainActivity.kt
+++ app/src/main/java/com/udemy/agecalc/MainActivity.kt
@@ -6,14 +6,12 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import java.sql.Timestamp
 import java.text.SimpleDateFormat
-import java.time.LocalDate
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private var dateField : TextView? = null
-    private var dateInMinutes : TextView? = null
+    private var dateInMinutesField : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,40 +19,46 @@ class MainActivity : AppCompatActivity() {
 
         val buttonDateSelector : Button = findViewById(R.id.button_date_selector)
         dateField = findViewById(R.id.date)
-        dateInMinutes = findViewById(R.id.result)
+        dateInMinutesField = findViewById(R.id.result)
 
         buttonDateSelector.setOnClickListener {
             dateSelection()
         }
     }
 
-    fun dateSelection() {
+    private fun dateSelection() {
         val calendar = Calendar.getInstance()
         val selectedYear: Int = calendar.get(Calendar.YEAR)
         val selectedMonth: Int = calendar.get(Calendar.MONTH)
         val selectedDay: Int = calendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener { view, year, month, day ->
+        val datePickerDialog = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { _, year, month, day ->
                 Toast.makeText(this, "selected date is $year-$month-$day", Toast.LENGTH_SHORT).show()
 
                 val date = "$year/${month + 1}/$day"
                 dateField?.text = date
 
                 val dateFormatter = SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH)
-                val parsedDateInMinutes = dateFormatter.parse(date).time / 60000
 
-                val currentDate = dateFormatter.parse(dateFormatter.format(System.currentTimeMillis()))
-                val currentDateInMinutes = currentDate.time / 60000
+                dateFormatter.parse(date)?.let { selectedDate ->
+                    val selectedDateInMinutes = selectedDate.time / 60000
 
-                val resultMinutes = currentDateInMinutes - parsedDateInMinutes
-
-                dateInMinutes?.text = resultMinutes.toString()
+                    dateFormatter
+                        .parse(dateFormatter.format(System.currentTimeMillis()))
+                        ?.let { currentDate ->
+                            val currentDateInMinutes = currentDate.time / 60000
+                            val resultMinutes = currentDateInMinutes - selectedDateInMinutes
+                            dateInMinutesField?.text = resultMinutes.toString()
+                        }
+                }
             },
             selectedYear,
             selectedMonth,
             selectedDay
-        ).show()
+        )
 
+        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+        datePickerDialog.show()
     }
 }
