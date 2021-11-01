1ce105587d8d23b54d66c5bacbd63946b6106ee2
lubamur <lubamuromskaya@gmail.com>
2021-11-01 16:09:13 +0300

    add minutes counting

--- app/src/main/java/com/udemy/agecalc/MainActivity.kt
+++ app/src/main/java/com/udemy/agecalc/MainActivity.kt
@@ -4,34 +4,56 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var dateField : TextView? = null
+    private var dateInMinutes : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val buttonDateSelector : Button = findViewById(R.id.button_date_selector)
+        dateField = findViewById(R.id.date)
+        dateInMinutes = findViewById(R.id.result)
 
         buttonDateSelector.setOnClickListener {
             dateSelection()
         }
     }
 
-    private fun dateSelection() {
+    fun dateSelection() {
         val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val selectedYear: Int = calendar.get(Calendar.YEAR)
+        val selectedMonth: Int = calendar.get(Calendar.MONTH)
+        val selectedDay: Int = calendar.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
             DatePickerDialog.OnDateSetListener { view, year, month, day ->
                 Toast.makeText(this, "selected date is $year-$month-$day", Toast.LENGTH_SHORT).show()
+
+                val date = "$year/${month + 1}/$day"
+                dateField?.text = date
+
+                val dateFormatter = SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH)
+                val parsedDateInMinutes = dateFormatter.parse(date).time / 60000
+
+                val currentDate = dateFormatter.parse(dateFormatter.format(System.currentTimeMillis()))
+                val currentDateInMinutes = currentDate.time / 60000
+
+                val resultMinutes = currentDateInMinutes - parsedDateInMinutes
+
+                dateInMinutes?.text = resultMinutes.toString()
             },
-            year,
-            month,
-            day
+            selectedYear,
+            selectedMonth,
+            selectedDay
         ).show()
 
     }
